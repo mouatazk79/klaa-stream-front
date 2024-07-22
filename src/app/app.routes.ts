16baef104195fdf2c6b8731d0a3ba2b4d22492e8
klaa <mouatazk79@gmail.com>
2024-07-22 20:19:55 +0100

    adding the routes of the new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { LoginComponent } from './authentication/components/login/login.componen
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './authentication/components/register/register.component';
 import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { DocumentListComponent } from './document/components/document-list/document-list.component';
+import { NotificationListComponent } from './notification/components/notification-list/notification-list.component';
 
 export const routes: Routes = [
     {path: 'login' ,component: LoginComponent},
     {path: 'home' ,component: HomeComponent},
     {path: 'register' ,component: RegisterComponent},
     {path: 'sidebar' ,component: SidebarComponent},
+    {path: 'documentlist' ,component: DocumentListComponent},
+    {path: 'notificationlist' ,component: NotificationListComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
