73f7db997f92418040e03f0d1abf36513abe84df
klaa <mouatazk79@gmail.com>
2024-07-25 16:37:25 +0100

    completing the login and register components

--- src/app/authentication/components/register/register.component.ts
+++ src/app/authentication/components/register/register.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule,RouterOutlet,RouterLink],
+  imports: [CommonModule,RouterOutlet,RouterLink,DividerModule, ButtonModule, InputTextModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
