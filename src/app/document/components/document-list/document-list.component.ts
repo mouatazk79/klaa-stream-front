57d720d77e7336c8296ccffaedb10a11941fda11
klaa <mouatazk79@gmail.com>
2024-09-05 00:20:57 +0100

    adding the document model and the document list component

--- src/app/document/components/document-list/document-list.component.ts
+++ src/app/document/components/document-list/document-list.component.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
@@ -8,24 +8,42 @@ import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from '../../../shared/components/sidebar/sidebar.component';
+import { GenericService, SERVICE_CONFIG } from '../../../shared/services/generic.service';
+import { Document } from '../../../shared/models/doument';
 @Component({
   selector: 'app-document-list',
   standalone: true,
   imports: [RouterOutlet,CommonModule,SidebarComponent,TableModule, InputTextModule, TagModule, IconFieldModule, InputIconModule],
   templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.scss'
+  styleUrl: './document-list.component.scss',
+  providers:[
+    GenericService,
+    {
+      provide: SERVICE_CONFIG,
+      useValue: { resourceEndpoint: 'documents' },
+    },
+  ]
 })
-export class DocumentListComponent {
-  documents!: null[];
+export class DocumentListComponent implements OnInit{
+  documents: Array<Document> = [];
+  constructor( private genericService: GenericService<Document, Document>
+  ) {}
 
-  selectedDocuments!: null;
-
-  constructor() {}
-
-  ngOnInit() {
-     // this.customerService.getCustomersMini().then((data) => (this.customers = data));
+  ngOnInit(): void {
+    this.getDocuments()
   }
-
+  getDocuments(){
+    this.genericService.getList().subscribe({
+      next:(data)=>{
+        this.documents = data.items || [];
+        console.log(data)
+      },
+      error:(err)=>{
+        console.log(err)
+      
+      }
+    })
+  }
   
 
 }
---
+++ src/app/shared/models/doument.ts
@@ -0,0 +1,6 @@
+export interface Document{
+    name:string,
+    Description?:string,
+    documentType:'BOOK'|'ARTICLE',
+    documentURL:string   
+}
